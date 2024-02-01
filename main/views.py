f18b94dd8db7c3dd2cf48a8e9210e56b4890f39f
dmitriy <modernhub@mail.ru>
2024-02-01 10:03:17 +0500

    first cascade app

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content' : 'Home page HOME',
+        'list': ['first', 'second'],
+        'dict': {'first' : 1},
+        'bool': True,
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
