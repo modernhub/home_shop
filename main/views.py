435b2d28c5dfc2b2b4421e61d55899d812220d36
dmitriy <modernhub@mail.ru>
2024-02-01 11:27:19 +0500

    finish app main

--- main/views.py
+++ main/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content' : 'Home page HOME',
-        'list': ['first', 'second'],
-        'dict': {'first' : 1},
-        'bool': True,
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели Home',
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page' : 'Учусь делать бэк'
+    }
+    return render(request, 'main/about.html', context)
