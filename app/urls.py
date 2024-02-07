dd2d4550d43951d83342041fc389deae7b06d164
dmitriy <modernhub@mail.ru>
2024-02-08 00:53:20 +0500

    added goods app

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
