f64a84356a2afb2e290aeb17c2eb0b11d86b2a32
dmitriy <modernhub@mail.ru>
2024-03-27 01:23:24 +0500

    added images

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,9 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
-from app.settings import DEBUG
+from app import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +12,8 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
