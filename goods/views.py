961542ad8a291b12deac647774fcaa554698502c
dmitriy <modernhub@mail.ru>
2024-04-22 15:34:48 +0500

    added pagination (get params)

--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
+from app.settings import GOODS_ON_PAGE
 
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, GOODS_ON_PAGE)
+    current_page = paginator.page(int(page))
+
     context = {
-        "title": "Home - Каталог",
-        "goods": goods,   
+        'title': 'Home - Каталог',
+        'goods': current_page,  
+        'slug_url':category_slug
     }
     return render(request, "goods/catalog.html", context=context)
 
